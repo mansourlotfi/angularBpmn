02c77ba29289676fe196b7aff0ebf988414310c9
mansourlotfi <mansourlotfi@gmail.com>
2020-02-01 22:10:50 +0330

    adding material element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'bpmn-js-angular';
-  diagramUrl = 'https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
-  importError?: Error;
+	opened: boolean;
+	title = 'bpmn-js-angular';
+	diagramUrl = 'https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
+	importError?: Error;
 
-  handleImported(event) {
+	handleImported(event) {
+		const { type, error, warnings } = event;
 
-    const {
-      type,
-      error,
-      warnings
-    } = event;
+		if (type === 'success') {
+			console.log(`Rendered diagram (%s warnings)`, warnings.length);
+		}
 
-    if (type === 'success') {
-      console.log(`Rendered diagram (%s warnings)`, warnings.length);
-    }
+		if (type === 'error') {
+			console.error('Failed to render diagram', error);
+		}
 
-    if (type === 'error') {
-      console.error('Failed to render diagram', error);
-    }
-
-    this.importError = error;
-  }
-
+		this.importError = error;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { DiagramComponent } from "./diagram/diagram.component";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatSliderModule } from "@angular/material/slider";
+import { BrowserModule } from '@angular/platform-browser';
+import { DiagramComponent } from './diagram/diagram.component';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [AppComponent, DiagramComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatSliderModule
-  ],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, DiagramComponent ],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		BrowserAnimationsModule,
+		MatSliderModule,
+		MatToolbarModule,
+		MatSidenavModule
+	],
+	bootstrap: [ AppComponent ]
 })
 export class AppModule {}
